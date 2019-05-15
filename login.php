3377c4924968a4f40fe73c8fa3d9697fa8de81f7
Roland Vägi <43164616+rolandvgi@users.noreply.github.com>
2019-05-15 14:14:39 +0300

    a

--- login.php
+++ login.php
@@ -1,4 +1,31 @@
-<?php require_once("includes/header.php"); ?>
+<?php require_once("includes/header.php");
+require_once("includes/base.php");
+
+$email = "";
+$emailError = "";
+$passwordError = "";
+$notice = "";
+
+if(isset($_POST["submit"])){
+	if (isset($_POST["email"]) and !empty($_POST["email"])){
+	  $email = $_POST["email"];
+    } else {
+	  $emailError = "Palun sisesta kasutajatunnusena e-posti aadress!";
+    }
+  
+    if (!isset($_POST["password"]) or strlen($_POST["password"]) < 8){
+	  $passwordError = "Palun sisesta parool, vähemalt 8 märki!";
+    }
+  
+  if(empty($emailError) and empty($passwordError)){
+	 $notice = signin($email, $_POST["password"]);
+     } else {
+	  $notice = "Ei saa sisse logida!";
+  }
+
+  }
+
+?>
 
 <div class="container mt-3">
     <h3>Logi sisse! </h3>
