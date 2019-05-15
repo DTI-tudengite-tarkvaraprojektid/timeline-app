b51eb04c9ad844c3dbbb58d975cdc23f9469da9f
KareVulli <m_helstein@hotmail.com>
2019-05-15 15:14:16 +0300

    Documented Response*.php classes

--- includes/Classes/Response.php
+++ includes/Classes/Response.php
@@ -2,7 +2,14 @@
 
 class Response {
 
+    /** 
+     * @var int HTTP status code
+     */
     private $status;
+
+    /**
+     * @var array Response data
+     */
     private $data;
 
     public function __construct($status, $data) {
@@ -10,23 +17,47 @@ class Response {
         $this->data = $data;
     }
 
+    /**
+     * Gives the HTTP status code
+     * 
+     * @return int HTTP status code
+     */
     public function getStatus()
     {
         return $this->status;
     }
 
+    /**
+     * Sets the HTTP status code
+     * 
+     * @param int $status HTTP Status code
+     * @return self
+     */
     public function setStatus($status)
     {
         $this->status = $status;
+        return $this;
     }
 
+    /**
+     * Gives the Response data
+     * 
+     * @return array Response data
+     */
     public function getData()
     {
         return $this->data;
     }
 
+    /**
+     * Sets the Response data
+     * 
+     * @param array $data Response data
+     * @return self
+     */
     public function setData($data)
     {
         $this->data = $data;
+        return $this;
     }
 }
--- includes/Classes/ResponseHelper.php
+++ includes/Classes/ResponseHelper.php
@@ -3,17 +3,36 @@
 require_once(__DIR__ . '/Response.php');
 
 class ResponseHelper {
+
+    /**
+     * Creates a response for sending an error message
+     * 
+     * @param string $message Error text sent as "message"
+     * @param int $status HTTP Status code
+     * @return \Response Response that can be sent using ResponseHelper::sendResponse();
+     */
     public static function getErrorResponse($message, $status = 400)
     {
         return new Response($status, ['message' => $message]);
     }
 
+    /**
+     * Creates an HTTP 200 response
+     * 
+     * @param array $data Response data that will be sent as JSON
+     * @return \Response Response that can be sent using ResponseHelper::sendResponse();
+     */
     public static function getResponse($data)
     {
         return new Response(200, $data);
     }
 
-    // This will stop the script
+    
+    /**
+     * Sends out the response and terminates the script.
+     * 
+     * @param \Response $response
+     */
     public static function sendResponse(Response $response)
     {
         header('Content-Type: application/json');
