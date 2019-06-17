bf79103b5509c7601565bda82ce70ec70be69f0f
tester <tester@mail.ee>
2019-06-17 09:54:21 +0300

    changed look of gallery

--- config/services.php
+++ config/services.php
@@ -33,5 +33,4 @@ return [
     'upload_path' => $app->getRootDir() . '/var/uploads',
     'thumbnail_path' => $app->getRootDir() . '/var/uploads/thumbnails',
     'file_upload_path' => $app->getRootDir() . '/public/files',
-    'file_upload_uri' => $_SERVER['APP_FILES_PATH']
 ];
