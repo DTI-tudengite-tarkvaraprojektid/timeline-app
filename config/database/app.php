f6d5ea14750f26a6b0d21a53692f9bdb6a91b30a
stevsal <ssaluri@tlu.ee>
2019-06-20 10:24:11 +0300

    fixed date range

--- config/database/app.php
+++ config/database/app.php
@@ -24,7 +24,7 @@ Manager::schema()->create('events', function (Blueprint $table) {
     $table->integer('timeline_id')->unsigned();
     $table->string('title', 255);
     $table->text('content');
-    $table->timestamp('time')->useCurrent();
+    $table->dateTime('time')->useCurrent();
     $table->timestamps();
     $table->softDeletes();
     $table->foreign('user_id')->references('id')->on('user');
